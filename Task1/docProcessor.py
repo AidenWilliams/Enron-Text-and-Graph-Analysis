cd09277f9bb07668fed0c02949ececdde6424392
ijtan <ethanzammit@gmail.com>
2021-02-07 23:46:58 +0100

    starting doc builder; snagged new parser and adapted

--- Task1/docProcessor.py
+++ Task1/docProcessor.py
@@ -1,2 +1,67 @@
-documents = []
+# from parser import myDict
+import json,os
+from tqdm import tqdm
+
+
+
+
+def docBuilder(mailboxes):
+    global docs
+    docs = {}
+    emails = mailboxes.keys()
+    for sender in tqdm(emails):
+        for email in mailboxes[sender]:
+            for recipient in email['tos']:
+                if sender==recipient:
+                    continue;
+                users = sender+recipient
+                usersR = recipient+sender
+
+                if docs.get(users) is None:
+                    if docs.get(usersR) is not None:
+                        tmp = users
+                        users = usersR
+                        usersR = tmp #useful to stop for later reference
+                    else:
+                        docs[users] = {}
+
+                if docs.get(users) is not None and docs.get(usersR) is not None:
+                    docs[users].update(docs[usersR])
+                    docs[usersR] = None
+ 
+
+                if docs[users].get('text') is None:
+                    docs[users]['text'] = ""
+
+                docs[users]['text']+=email['text']
+
+
+
+
+
+
+def checkDuplicates():
+    print('checking doubles')
+    for emailA in mailboxes.keys():
+        for emailB in mailboxes.keys():
+            if (emailA+emailB) in mailboxes and (emailB+emailA) in mailboxes:
+                print('DUPLICATE FOUND!')
+    print('done checking doubles')
+
+
+def save(data,path):
+    print('saving')
+    with open(path,'w') as f:
+        json.dump(data,f,indent=4)
+
+
+path = os.path.join('intermediary', 'mailboxes.json')
+with open(path) as f:
+    mailboxes = json.load(f)
+
+docBuilder(mailboxes)
+checkDuplicates()
+path = os.path.join('intermediary', 'documents.json')
+save(docs,path)
+
 
---
+++ Task1/parser2.py
@@ -0,0 +1,75 @@
+import os
+from email.parser import Parser
+from tqdm import tqdm
+import json
+
+rootDir = os.path.join('data', 'maildir')
+myDict = {}
+
+
+def readMail(email):
+    update = {'subject': email['subject'],
+              'text': email.get_payload(), 'tos': []}
+
+    if email['to']:
+        email_to = email['to']
+        email_to = email_to.replace("\n", "")
+        email_to = email_to.replace("\t", "")
+        email_to = email_to.replace(" ", "")
+        update['tos'] = list(set(email_to.split(',')))
+    return update
+
+
+def addMail(dic, emailDir):
+    with open(emailDir, "r") as f:
+        email = Parser().parsestr(f.read())
+        key = email['from']
+        if key in dic:
+            value = dic.get(email['from'])
+            value.append(readMail(email))
+            dic.update({key: value})
+        else:
+            value = [readMail(email)]
+            dic.update({key: value})
+
+
+def addAll(dic, emailDir):
+    if not os.path.isfile(emailDir):
+        for file in os.listdir(emailDir):
+            addAll(dic, os.path.join(emailDir, file))
+    else:
+        addMail(dic, emailDir)
+
+
+def loadData(dic, root):
+    for user in tqdm(os.listdir(root)):
+        userPath = os.path.join(root, user)
+        for folder in os.listdir(userPath):
+            folderPath = os.path.join(root, user, folder)
+            if os.path.isfile(folderPath):
+                addMail(dic, folderPath)
+                continue
+
+            for file in os.listdir(folderPath):
+                addAll(dic, os.path.join(rootDir, user, folder, file))
+
+
+def _saveToFile(data, path):
+    print('Saving')
+    with open(path, 'w') as fp:
+        json.dump(data, fp, indent=4)
+
+
+loadData(myDict, rootDir)
+path = "/intermediary/mailboxes.json"
+
+if os.path.isfile(path) and os.access(path, os.R_OK):
+    print("Docs file found!")
+    print('Reading...')
+    with open(path, 'r') as f:
+        docs = json.load(f)
+
+else:
+    print("Either file is missing or is not readable, creating file...")
+    #docs = _docBuilder(mailboxes)
+    _saveToFile(myDict, path=path)

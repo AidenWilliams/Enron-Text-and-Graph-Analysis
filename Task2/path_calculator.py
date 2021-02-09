1844cb92f0cded52d893c938384e8b63d557cd13
Aiden <mw3dbest@gmail.com>
2021-02-09 18:54:23 +0100

    subgraph sadness, will do graph for each folder

--- Task2/path_calculator.py
+++ Task2/path_calculator.py
@@ -1,82 +1,4 @@
-
 from tqdm import tqdm
-# graph is the networkx graph
-# edges is the dictionary of tuples as keys and weights as values created when building the mention graph
-def get_average_path_length(graph, edges):
-    # define vertices as a list of the graph's nodes
-    vertices = list(graph.nodes)
-    # Get the basic 1 distance paths by getting the tuple in edges and paring it with 1
-    paths = {e: 1 for e in edges}
-    # paths key will be tuple source to destination: value is length of path
-    #                        (source,   destination): p_length
-
-    # start iterating over all the vertices
-    for v1 in tqdm(vertices):
-        # use degrees to make process faster
-        # if the vertex doesnt have an out degree then don't bother trying to build paths emerging from it as there are none!
-        # if the vertex is with the finished set, then its paths have already been added to paths
-        if graph.out_degree(v1) == 0 or v1 in finished:
-            # if it isn't added to the finished set add it
-            if v1 not in finished:
-                finished.add(v1)
-            continue
-        # build paths for v1
-        build_paths(graph, paths, v1)
-        # add v1 to the finished set
-        finished.add(v1)
-
-    # Add all the distances in paths' values
-    sum = 0
-    for distance in paths.values():
-        sum += distance
-    #       1
-    # -------------
-    # |V|^2 - |V|
-    lG = \
-        1 \
-        / (len(vertices) * len(vertices) - len(vertices))
-    # return the average path length
-    return float(lG * sum)
-
-
-# graph is the networkx graph
-# edges is the dictionary of tuples as keys and weights as values created when building the mention graph
-def get_diameter(graph, edges):
-    # define vertices as a list of the graph's nodes
-    vertices = list(graph.nodes)
-    # Get the basic 1 distance paths by getting the tuple in edges and paring it with 1
-    paths = {e: 1 for e in edges}
-    # paths key will be tuple source to destination: value is length of path
-    #                        (source,   destination): p_length
-
-    # start iterating over all the vertices
-    for v1 in tqdm(vertices):
-        # use degrees to make process faster
-        # if the vertex doesnt have an out degree then don't bother trying to build paths emerging from it as there are none!
-        # if the vertex is with the finished set, then its paths have already been added to paths
-        if graph.out_degree(v1) == 0 or v1 in finished:
-            # if it isn't added to the finished set add it
-            if v1 not in finished:
-                finished.add(v1)
-            continue
-        # build paths for v1
-        build_paths(graph, paths, v1)
-        # add v1 to the finished set
-        finished.add(v1)
-
-    # Add all the distances in paths' values
-    shortest = 0
-    for distance in paths.values():
-        if distance > shortest:
-            shortest = distance
-
-    return shortest
-
-
-# Holds a set of all the vertices that have had their paths created
-finished = set()
-# Holds a set of tuples of 2 vertices indicating there is a loop between both vertices
-looped = set()
 
 
 # graph is the networkx graph
@@ -86,7 +8,7 @@ looped = set()
 # p_length is the path length so far (to be saved as the value for paths)
 # seen is a set of all the vertices visited while building these paths
 # looping is a boolean variable that indicates whether the function has been stuck in a loop or not
-def build_paths(graph, paths, origin, v2=None, p_length=1, seen=None, looping=False):
+def build_paths(graph, paths, origin, finished, looped, v2=None, p_length=1, seen=None, looping=False):
     if seen is None:
         seen = set()
     # get a list of all the vertices connected to v1 (directly)
@@ -127,7 +49,7 @@ def build_paths(graph, paths, origin, v2=None, p_length=1, seen=None, looping=Fa
                 continue
             # if yes (tuple already in paths)
             # if the new path length is less than the old length
-            if  (p_length - 1) < paths.get(t):
+            if (p_length - 1) < paths.get(t):
                 # Update only the value/ path length
                 # I subtract 1 here for the same reason as before
                 paths.update({t: p_length - 1})
@@ -191,4 +113,85 @@ def build_paths(graph, paths, origin, v2=None, p_length=1, seen=None, looping=Fa
                 # v2 is now the current vertex
                 # p_length is incremented by 1
                 # looping is updated
-                build_paths(graph, paths, origin, v2=vertex, p_length=p_length + 1, seen=seen, looping=b)
+                build_paths(graph, paths, origin, finished, looped,  v2=vertex, p_length=p_length + 1, seen=seen,
+                            looping=b)
+
+# graph is the networkx graph
+# edges is the dictionary of tuples as keys and weights as values created when building the mention graph
+def get_average_path_length(graph, edges):
+    # Holds a set of all the vertices that have had their paths created
+    finished = set()
+    # Holds a set of tuples of 2 vertices indicating there is a loop between both vertices
+    looped = set()
+    # define vertices as a list of the graph's nodes
+    vertices = list(graph.nodes)
+    # Get the basic 1 distance paths by getting the tuple in edges and paring it with 1
+    paths = {e: 1 for e in edges}
+    # paths key will be tuple source to destination: value is length of path
+    #                        (source,   destination): p_length
+
+    # start iterating over all the vertices
+    for v1 in tqdm(vertices):
+        # use degrees to make process faster
+        # if the vertex doesnt have an out degree then don't bother trying to build paths emerging from it as there are none!
+        # if the vertex is with the finished set, then its paths have already been added to paths
+        if graph.out_degree(v1) == 0 or v1 in finished:
+            # if it isn't added to the finished set add it
+            if v1 not in finished:
+                finished.add(v1)
+            continue
+        # build paths for v1
+        build_paths(graph, paths, v1, finished, looped)
+        # add v1 to the finished set
+        finished.add(v1)
+
+    # Add all the distances in paths' values
+    sum = 0
+    for distance in paths.values():
+        sum += distance
+    #       1
+    # -------------
+    # |V|^2 - |V|
+    lG = \
+        1 \
+        / (len(vertices) * len(vertices) - len(vertices))
+    # return the average path length
+    return float(lG * sum)
+
+
+# graph is the networkx graph
+# edges is the dictionary of tuples as keys and weights as values created when building the mention graph
+def get_diameter(graph, edges):
+    # Holds a set of all the vertices that have had their paths created
+    finished = set()
+    # Holds a set of tuples of 2 vertices indicating there is a loop between both vertices
+    looped = set()
+    # define vertices as a list of the graph's nodes
+    vertices = list(graph.nodes)
+    # Get the basic 1 distance paths by getting the tuple in edges and paring it with 1
+    paths = {e: 1 for e in edges}
+    # paths key will be tuple source to destination: value is length of path
+    #                        (source,   destination): p_length
+
+    # start iterating over all the vertices
+    for v1 in tqdm(vertices):
+        # use degrees to make process faster
+        # if the vertex doesnt have an out degree then don't bother trying to build paths emerging from it as there are none!
+        # if the vertex is with the finished set, then its paths have already been added to paths
+        if graph.out_degree(v1) == 0 or v1 in finished:
+            # if it isn't added to the finished set add it
+            if v1 not in finished:
+                finished.add(v1)
+            continue
+        # build paths for v1
+        build_paths(graph, paths, v1, finished, looped)
+        # add v1 to the finished set
+        finished.add(v1)
+
+    # Add all the distances in paths' values
+    shortest = 0
+    for distance in paths.values():
+        if distance > shortest:
+            shortest = distance
+
+    return shortest
